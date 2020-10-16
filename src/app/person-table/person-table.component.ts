c8ab83b4af736431270a77bd19814f89b804adf2
Gilian-S <mynameisguzse@gmail.com>
2020-10-16 14:39:43 +0200

    getPersons finished

--- src/app/person-table/person-table.component.ts
+++ src/app/person-table/person-table.component.ts
@@ -1,15 +1,18 @@
+import { PersonService, IPerson } from './../person.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-person-table',
   templateUrl: './person-table.component.html',
-  styleUrls: ['./person-table.component.scss']
+  styleUrls: ['./person-table.component.scss'],
 })
 export class PersonTableComponent implements OnInit {
-
-  constructor() { }
-
+  persons: IPerson[];
+  constructor(private personService: PersonService){}
   ngOnInit(): void {
+	  this.personService.getPersons().subscribe((data: IPerson[]) => {
+		  console.log(data);
+		  this.persons = data;
+	  })
   }
-
 }
--- src/app/person.service.ts
+++ src/app/person.service.ts
@@ -13,14 +13,36 @@ export class PersonService {
   }
 
   getPerson(id: number) {
-    return this.http.get(`${this.URL}/${id}`);
+    return this.http.get<IPerson>(`${this.URL}/${id}`);
   }
 
   deletePerson(id: number) {
     return this.http.delete(`${this.URL}/${id}`);
   }
 
-  postPerson(person) {
+  postPerson(person:IPerson) {
     return this.http.post(this.URL, person);
   }
+}
+export interface Address {
+  id: number;
+  street: string;
+  postcode: string;
+  city: string;
+  country: string;
+}
+
+export interface IPerson {
+  id: number;
+  name: string;
+  lastName: string;
+  minor: boolean;
+  resident: boolean;
+  email: string;
+  nationalNumber: string;
+  address: Address;
+  birthPlace: string;
+  dateOfBirth: string;
+  maritalStatus: string;
+  partnerName: string;
 }
